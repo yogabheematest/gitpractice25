6960a12bc1fc83768d80e5ce074dfe0744ae5266
yogabheematest <yogabheematest@gmail.com>
2023-01-25 06:11:14 +0530

    new project uploading to the repo

---
+++ src/test/java/javaDemos/testClass.java
@@ -0,0 +1,12 @@
+package javaDemos;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing....");
+
+	}
+
+}
